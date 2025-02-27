18afd41c60c8366db270465ff26a569840738c08
Mark Phillips <mark_phillips85@hotmail.com>
2025-02-27 19:42:35 +0000

    [ReadAloud] Add more pronunciations and update variable name

--- PowerShell/ReadAloudArseblogLive.ps1
+++ PowerShell/ReadAloudArseblogLive.ps1
@@ -40,22 +40,24 @@ while ($ExitCountdown -gt 0) {
             $UpdateText = $LiveBlogUpdatesArray[$i]
             if ($UpdateText -ne "") {
                 # Fix pronunciations
-                $UpdateSpoken = $UpdateText
-                $UpdateSpoken = $UpdateSpoken -replace "Patreon","Paitreeon" -replace "\bFT\b","Full time" -replace "fk","Free kick" -replace 'Oo[o]+h',"Woah" -replace 'Go[oa]+al',"Goal"
-                $UpdateSpoken = $UpdateSpoken -replace "Mikel","Mickel" -replace "Arteta","Artetuh" -replace "Arsenal","Arsnal"
-                $UpdateSpoken = $UpdateSpoken -replace "Nwaneri","Nwan-airy" -replace "Gabriel","Gab-ri-ell" -replace "Raya","Rye-uh" -replace "Calafiori","Cal-afewer-ray" -replace "Jorginho","Jor-gene-yo" -replace "Saka","Sack-uh" -replace "Havertz","Hav-urtz"
-                $UpdateSpoken = $UpdateSpoken -replace "Kiwior","Key-vee-or" -replace "Zinchenko","Zin-chenko" -replace "Saliba","Sahleeba"
-                $UpdateSpoken = $UpdateSpoken -replace "\bOde\b", "Odegaard" -replace "\bMLS\b", "Lewis-Skelly" -replace "\bTross\b","Trossard"
+                $UpdatedSpoken = $UpdateText
+                $UpdatedSpoken = $UpdatedSpoken -replace "Patreon","Paitreeon" -replace "\bFT\b","Full time" -replace "fk","Free kick" -replace 'Oo[o]+h',"Woah" -replace 'Go[oa]+al',"Goal"
+                $UpdatedSpoken = $UpdatedSpoken -replace "Mikel","Mickel" -replace "Arteta","Artetuh" -replace "Arsenal","Arsnal"
+                $UpdatedSpoken = $UpdatedSpoken -replace "Nwaneri","Nwan-airy" -replace "Gabriel","Gab-ri-ell" -replace "Raya","Rye-uh" -replace "Calafiori","Cal-afewer-ray" -replace "Jorginho","Jor-gene-yo" -replace "Saka","Sack-uh" -replace "Havertz","Hav-urtz"
+                $UpdatedSpoken = $UpdatedSpoken -replace "Kiwior","Key-vee-or" -replace "Zinchenko","Zin-chenko" -replace "Saliba","Sahleeba"
+                $UpdatedSpoken = $UpdatedSpoken -replace "\bOde\b", "Odegaard" -replace "\bMLS\b", "Lewis-Skelly" -replace "\bTross\b","Trossard" -replace "\bKT\b", "Kieran Tierney" -replace "Zinky","Zin-chenko"
    
                 # Omit the time element if it matches the last one
-                $TimeValue = ($UpdateSpoken -split " :: ")[0]
+                $TimeValue = ($UpdatedSpoken -split " :: ")[0]
                 # Write-Host "    DEBUG: TimeValue: $TimeValue"
                 if ($TimeValue -eq $PreviousTime) {
-                    $UpdateSpoken = $UpdateSpoken -replace "$TimeValue :: ",""
+                    $UpdatedSpoken = $UpdatedSpoken -replace "$TimeValue :: ",""
                 } elseif ($TimeValue -match '^\d+$') {
-                    $UpdateSpoken = $UpdateSpoken -replace "$TimeValue :: ","$TimeValue minutes. "
+                    $UpdatedSpoken = $UpdatedSpoken -replace "$TimeValue :: ","$TimeValue minutes. "
+                } elseif ($TimeValue -eq 'Half time' -or $TimeValue -eq 'Full time') {
+                    $UpdatedSpoken = $UpdatedSpoken -replace "$TimeValue :: ",""
                 } else {
-                    $UpdateSpoken = $UpdateSpoken -replace "$TimeValue :: ","$TimeValue. "
+                    $UpdatedSpoken = $UpdatedSpoken -replace "$TimeValue :: ","$TimeValue. "
                 }
 
                 $UpdateText = $UpdateText -replace " :: ", ". "
@@ -73,8 +75,8 @@ while ($ExitCountdown -gt 0) {
                 # Write-Host "[$(Get-Date -Format "HH:mm:ss"), (#:$LatestUpdatesCount)] $i --> $UpdateText"
                 # Write-Host "[$(Get-Date -Format "HH:mm:ss"), (#:$("{0:D3}" -f $LatestUpdatesCount))] $("{0:D3}" -f $i) --> $UpdateText"
                 Write-Host ("[$(Get-Date -Format "HH:mm:ss"), (#:{0:D3})] {1:D3}) --> $UpdateText" -f $LatestUpdatesCount,$i)
-                # Write-Host "    DEBUG: $UpdateSpoken"
-                $Speech.Speak($UpdateSpoken)
+                # Write-Host "    DEBUG: $UpdatedSpoken"
+                $Speech.Speak($UpdatedSpoken)
                 start-sleep -seconds 1
             }
             $UpdatesReadOutCount++
